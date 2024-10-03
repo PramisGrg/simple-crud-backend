7ee16d226d699c2bcfa817cdd1bb14a476b8d475
PramisGrg <gurungpramis361@gmail.com>
2024-10-03 21:58:28 +0545

    feat: delete products

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from "express";
 import { ProductModel } from "../models/product";
 
+//get all products
 export const getAllProducts = async (req: Request, res: Response) => {
   try {
     const allProducts = await ProductModel.find({});
@@ -12,6 +13,7 @@ export const getAllProducts = async (req: Request, res: Response) => {
   }
 };
 
+//add product
 export const addProduct = async (req: Request, res: Response) => {
   try {
     const addProduct = await ProductModel.create(req.body);
@@ -20,5 +22,20 @@ export const addProduct = async (req: Request, res: Response) => {
     if (error instanceof Error) {
       res.status(500).json({ error: "An unkonwn error occured" });
     }
+  }
+};
+
+//delete product
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deleteProduct = await ProductModel.findByIdAndDelete(id);
+    if (deleteProduct) {
+      res.status(200).json({ message: "Product deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "An unexpected error occured" });
   }
 };
